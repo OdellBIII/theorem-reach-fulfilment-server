de03954ec71283fd1728b86ff6c0399f9ebc9f8a
Oblackmon <oblackmo@andrew.cmu.edu>
2020-06-27 21:39:01 -0400

    printing JSON

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ http.createServer(function (req, res) {
 
 		req.on('end', () => {
 			console.log(JSON.parse(data));
-		})
+		});
 		res.writeHead(200, {'Content-Type' : 'text/html'});
 		res.end();
 	}
