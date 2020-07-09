7b4c63a76db3c6f741ccd688ab5a560c4e49e765
Oblackmon <oblackmo@andrew.cmu.edu>
2020-07-09 19:49:02 -0400

    Parsing data

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ http.createServer(function (req, res) {
 		});
 
 		req.on('end', () => {
-			console.log(JSON.parse(data));
+			console.log(data;
 		});
 		res.writeHead(200, {'Content-Type' : 'text/html'});
 		res.end();
