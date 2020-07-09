e574cf3c28b62019b06eb7dd3a546cdcb602a69f
Oblackmon <oblackmo@andrew.cmu.edu>
2020-07-09 19:58:45 -0400

    Trying webhooks using express

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
+app.post("/hook", (req, res) => {
+
+  console.log(req.body);
+  res.status(200).end();
+});
