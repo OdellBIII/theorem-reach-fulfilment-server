63bbfdab0c62f15d60a2c5ccecfc309b1acdab0e
Oblackmon <oblackmo@andrew.cmu.edu>
2020-07-09 20:40:18 -0400

    Switched put to get

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 const firebase = require('firebase/app');
 require('firebase/database')
-firebase.initializaeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 const express = require('express');
