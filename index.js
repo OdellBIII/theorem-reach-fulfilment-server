6b28d219d9d5416693a91fe21435f2f970fdcf37
Oblackmon <oblackmo@andrew.cmu.edu>
2020-07-09 20:32:12 -0400

    Switched put to get

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDhQObZf4mIW6HlLsnOdSyVWIRMipUSkeg",
+  authDomain: "sparechange-80a62.firebaseapp.com",
+  databaseURL: "https://sparechange-80a62.firebaseio.com",
+  projectId: "sparechange-80a62",
+  storageBucket: "sparechange-80a62.appspot.com",
+  messagingSenderId: "506295831986",
+  appId: "1:506295831986:web:47d707b07de7bd5689a8ab",
+  measurementId: "G-LB8GHE413R"
+};
+
+firebase.initializaeApp(config);
+
+var database = firebase.database();
 const express = require('express');
 
 const bodyParser = require("body-parser");
@@ -14,8 +28,14 @@ app.set('port', process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
 
-  console.log("Webhook recognized")
+  console.log("Webhook recognized");
   console.log(req.query);
+  var user = req.query;
+  database.ref('users/' + user.user_id).set({
+    tokens : query.reward,
+  });
+
+
   res.sendStatus(200);
 });
 
