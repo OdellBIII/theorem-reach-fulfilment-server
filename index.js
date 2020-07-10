b727b88ebcf6d61fb04e130078b8b04583d3fdab
Oblackmon <oblackmo@andrew.cmu.edu>
2020-07-09 20:14:29 -0400

    Trying webhook with Express

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('port', process.env.PORT || 3000);
 app.post("/", (req, res) => {
 
   console.log(req.body);
-  res.status(200).end();
+  res.status(200).send('OK');
 });
 
 var server = app.listen(app.get('port'), function() {
