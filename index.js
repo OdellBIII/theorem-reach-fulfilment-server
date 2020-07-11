698c7a123acd17869d3f21b942f449d497f97bd4
Oblackmon <oblackmo@andrew.cmu.edu>
2020-07-10 23:52:05 -0400

    Trying to read from database

--- index.js
+++ index.js
@@ -35,11 +35,10 @@ app.get("/", (req, res) => {
     console.log("Snapshot:");
     console.log(snapshot.val());
 
-    database.ref('users/' + user.user_id).update({tokens : snapshot.val().tokens + user.reward}, function(error) {
+    database.ref('users/' + user.user_id).update({tokens : parseInt(snapshot.val().tokens) + user.reward}, function(error) {
     if (error) {
       console.log(error);
     }else{
-      console.log(error);
       console.log("Successful write operation!")
     }
   });
